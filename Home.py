706c27a18c014d15d99816db46c99d8894389501
Stefan Reischl <reischl13@outlook.com>
2023-03-10 09:46:40 +0100

    Initialisierung

---
+++ Home.py
@@ -0,0 +1,25 @@
+import time
+import streamlit as st
+import datetime
+
+
+def fastenzeit(startzeit):
+    jetzt = datetime.datetime.now()
+    delta = jetzt - startzeit
+    tage = delta.days
+    stunden, rest = divmod(delta.seconds, 3600)
+    minuten, sekunden = divmod(rest, 60)
+    st.write("Du fastest seit {} Tagen, {} Stunden, {} Minuten und {} Sekunden".format(tage, stunden, minuten, sekunden))
+
+
+def main():
+    st.title("Fastenzeitrechner")
+    startzeit = datetime.datetime(2023, 3, 8, 18, 0, 0)
+    while True:
+        fastenzeit(startzeit)
+        time.sleep(1)
+        st.experimental_rerun()
+
+
+if __name__ == "__main__":
+    main()
